6059492e7d7775cd26b8575ab1a936e3e916e2de
Karankevich <liza_minyaeva@mail.ru>
2023-11-04 14:43:49 +0300

    fixing permissions for UserViewSet

--- users/views.py
+++ users/views.py
@@ -2,6 +2,7 @@ from rest_framework.viewsets import ModelViewSet
 from users.models import User
 from users.serializers import UserSerializer, UserCreateSerializer
 from rest_framework.response import Response
+from rest_framework.permissions import IsAdminUser, AllowAny
 
 
 class UserViewSet(ModelViewSet):
@@ -16,3 +17,10 @@ class UserViewSet(ModelViewSet):
             return Response(serializer.data)
         else:
             return Response(serializer.errors)
+
+    def get_permissions(self):
+        if self.action != 'create':
+            permission_classes = [IsAdminUser]
+        else:
+            permission_classes = [AllowAny]
+        return [permission() for permission in permission_classes]
